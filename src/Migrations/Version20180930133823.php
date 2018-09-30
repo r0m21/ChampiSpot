e24d33b2157ac2d478c9754395f103909ec8966c
Scalou21 <jerome.rouchy@hotmail.fr>
2018-09-30 16:40:57 +0200

    recuperation de fichiers

--- src/Migrations/Version20180930133823.php
+++ src/Migrations/Version20180930133823.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180826133617 extends AbstractMigration
+final class Version20180930133823 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
@@ -17,7 +17,9 @@ final class Version20180826133617 extends AbstractMigration
 
         $this->addSql('ALTER TABLE signalement DROP FOREIGN KEY FK_F4B5511443A014AB');
         $this->addSql('DROP INDEX IDX_F4B5511443A014AB ON signalement');
-        $this->addSql('ALTER TABLE signalement DROP sig_id_spot_id_id');
+        $this->addSql('ALTER TABLE signalement CHANGE sig_id_spot_id_id spot_id INT NOT NULL');
+        $this->addSql('ALTER TABLE signalement ADD CONSTRAINT FK_F4B551142DF1D37C FOREIGN KEY (spot_id) REFERENCES spot (id)');
+        $this->addSql('CREATE INDEX IDX_F4B551142DF1D37C ON signalement (spot_id)');
     }
 
     public function down(Schema $schema) : void
@@ -25,8 +27,10 @@ final class Version20180826133617 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE signalement ADD sig_id_spot_id_id INT NOT NULL');
-        $this->addSql('ALTER TABLE signalement ADD CONSTRAINT FK_F4B5511443A014AB FOREIGN KEY (sig_id_spot_id_id) REFERENCES spot (id)');
+        $this->addSql('ALTER TABLE signalement DROP FOREIGN KEY FK_F4B551142DF1D37C');
+        $this->addSql('DROP INDEX IDX_F4B551142DF1D37C ON signalement');
+        $this->addSql('ALTER TABLE signalement CHANGE spot_id sig_id_spot_id_id INT NOT NULL');
+        $this->addSql('ALTER TABLE signalement ADD CONSTRAINT FK_F4B5511443A014AB FOREIGN KEY (sig_id_spot_id_id) REFERENCES spot (id) ON DELETE CASCADE');
         $this->addSql('CREATE INDEX IDX_F4B5511443A014AB ON signalement (sig_id_spot_id_id)');
     }
 }
--- src/Migrations/Version20180930134052.php
+++ src/Migrations/Version20180930134052.php
@@ -8,14 +8,16 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20180826133214 extends AbstractMigration
+final class Version20180930134052 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE signalement ADD spot_id INT NOT NULL');
+        $this->addSql('ALTER TABLE signalement DROP FOREIGN KEY FK_F4B5511443A014AB');
+        $this->addSql('DROP INDEX IDX_F4B5511443A014AB ON signalement');
+        $this->addSql('ALTER TABLE signalement CHANGE sig_id_spot_id_id spot_id INT NOT NULL');
         $this->addSql('ALTER TABLE signalement ADD CONSTRAINT FK_F4B551142DF1D37C FOREIGN KEY (spot_id) REFERENCES spot (id)');
         $this->addSql('CREATE INDEX IDX_F4B551142DF1D37C ON signalement (spot_id)');
     }
@@ -27,6 +29,8 @@ final class Version20180826133214 extends AbstractMigration
 
         $this->addSql('ALTER TABLE signalement DROP FOREIGN KEY FK_F4B551142DF1D37C');
         $this->addSql('DROP INDEX IDX_F4B551142DF1D37C ON signalement');
-        $this->addSql('ALTER TABLE signalement DROP spot_id');
+        $this->addSql('ALTER TABLE signalement CHANGE spot_id sig_id_spot_id_id INT NOT NULL');
+        $this->addSql('ALTER TABLE signalement ADD CONSTRAINT FK_F4B5511443A014AB FOREIGN KEY (sig_id_spot_id_id) REFERENCES spot (id) ON DELETE CASCADE');
+        $this->addSql('CREATE INDEX IDX_F4B5511443A014AB ON signalement (sig_id_spot_id_id)');
     }
 }
